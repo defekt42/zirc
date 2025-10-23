88e188f12a6860b127bc072cbcc9c47949e8f99d
defekt42 <deadshell@gmail.com>
2025-10-23 11:03:37 -0400

    Modify startzirc.sh to use stable version and adjust tmux
    Updated the script to use zirc-sec-stable-v1.7 and changed the tmux split window size.

--- startzirc.sh
+++ startzirc.sh
@@ -2,7 +2,7 @@
 
 tmux split-window -t zirc -v -l 1 ~/.suckless/irc/zirc-sec/./zircin.sh #changed 2 to 1
 # added stty -echo; to kill double post
-stty -echo; ~/.suckless/irc/zirc-sec/./zirc-sec-w1-6 irc.libera.chat 6697 defekt prompt
+stty -echo; ~/.suckless/irc/zirc-sec/./zirc-sec-stable-v1.7 irc.libera.chat 6697 defekt prompt
 
 #~/.suckless/irc/./circ zirc-posix zirc-posix 127.0.0.1 6697 "##" "dh00l422" #
 
